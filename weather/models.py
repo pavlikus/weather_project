e6e8fa117285268c3f1ee64301e44eb0dd2be98e
pavlik <readystores.ru@gmail.com>
2023-12-23 14:29:17 +0300

    feat(weather): add model

--- weather/models.py
+++ weather/models.py
@@ -1,1 +1,17 @@
-# Create your models here.
+from typing import Self
+
+from django.db import models
+
+
+class City(models.Model):
+    """
+    TODO: add GIS system(DB based on GIS) if necessary
+    https://docs.djangoproject.com/en/5.0/ref/contrib/gis/
+    """
+
+    name = models.CharField(max_length=128, unique=True, db_index=True)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self: Self) -> str:
+        return self.name
